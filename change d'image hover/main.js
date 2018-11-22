6078ee1e9c408006fbf6350789f4204012d2b824
Lcunault <44022285+Lcunault@users.noreply.github.com>
2018-11-22 09:29:32 +0100

    change d'image hover

---
+++ change d'image hover/main.js
@@ -0,0 +1,13 @@
+// var top = document.getElementById("top");
+// var bottom = document.getElementById("bottom");
+// top.addEventListener("click", function() {
+//   top.classList.toggle("show");
+// })
+// bottom.addEventListener("click", function() {
+//   bottom.classList.toggle("show");
+// });
+
+var cf = document.getElementById('cf');
+cf.addEventListener('click', function () {
+  cf.classList.toggle('show');
+});
